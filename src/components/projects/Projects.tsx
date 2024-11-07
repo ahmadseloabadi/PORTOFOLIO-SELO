b86212010f91f08b6e6acdb5b45364c23f1db477
selo <ahmadseloabadi@gmail.com>
2024-11-07 22:49:06 +0700

    up: content in projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -4,14 +4,26 @@ import { ProjectProps } from "./Types";
 const Projects = () => {
   const projects: ProjectProps[] = [
     {
-      title: "Sentiment Analysis Myxl",
+      title: "Sentiment Analysis Myxl App Reviews",
       description:
         "An application that analyzes user sentiment from MyXL reviews on Google Play Store. Using a Genetic Algorithm-optimized SVM model with SMOTE for handle imbalance dataset, it classifies reviews as positive, negative, or neutral, helping improve service quality and user experience.",
       image: "/assets/ansen-gasvm-myxl.png",
       technologies: ["Streamlit", "Python", "Algorithma Genetika", "SVM"],
       category: ["Machine Learning", "Analysis Sentiment"],
-      demoUrl: "#",
+      demoUrl: "https://smote-svm-ga-9xbdvfjcqcnk6zbyespptz.streamlit.app/",
       githubUrl: "https://github.com/ahmadseloabadi/SMOTE-SVM-GA",
+      features: ["High Accuration", "Detection New Data", "Simple Design"],
+    },
+    {
+      title: "Sentiment Analysis Kredivo App Reviews",
+      description:
+        "An application that analyzes user sentiment from Kredivo reviews on Google Play Store. Using a Genetic Algorithm-optimized SVM model, it classifies reviews as positive, negative, or neutral, helping improve service quality and user experience.",
+      image: "/assets/ansen-gasvm-kredivo.png",
+      technologies: ["Streamlit", "Python", "Algorithma Genetika", "SVM"],
+      category: ["Machine Learning", "Analysis Sentiment"],
+      demoUrl: "https://w5zw4dqpwfsmbhplfqzblc.streamlit.app/",
+      githubUrl:
+        "https://github.com/ahmadseloabadi/analisis-sentimen-kredivo-GA-SVM",
       features: ["High Accuration", "Detection New Data", "Simple Design"],
     },
     {
@@ -34,6 +46,17 @@ const Projects = () => {
       features: ["High Accuration", "Detection New Data", "Simple Design"],
     },
     {
+      title: "Manggo Image Classification",
+      description:
+        "This project is a classification system designed to identify different types of mangoes using a Convolutional Neural Network (CNN) model. The CNN is trained on a dataset of mango images, learning to recognize unique  color for accurate classification.",
+      image: "/assets/cnn-mangga.png",
+      technologies: ["Python", "CNN", "Flask", "HTML", "CSS", "JavaScript"],
+      category: ["Machine Learning", "Img Processing"],
+      demoUrl: "#",
+      githubUrl: "https://github.com/ahmadseloabadi/klasifikasi-mangga-CNN",
+      features: ["High Accuration", "Detection New Data", "Simple Design"],
+    },
+    {
       title: "Recommendations Restaurant Yogyakarta",
       description:
         "An application providing personalized restaurant recommendations in Yogyakarta using hybrid filtering. By combining content-based and collaborative filtering methods, it delivers tailored suggestions based on user preferences and similar user behaviors, enhancing dining experiences across the city.",
@@ -41,13 +64,41 @@ const Projects = () => {
       technologies: ["Streamlit", "Python", "Hybrid filtering"],
 
       category: ["Machine Learning", "Recomendation"],
-      demoUrl: "#",
-      githubUrl: "#",
+      demoUrl:
+        "https://recsys-restaurant-app-ezv93g3cu7yuazxuzdv7vi.streamlit.app/",
+      githubUrl:
+        "https://github.com/ahmadseloabadi/recsys-restaurant-streamlit",
       features: [
         "Simple Desain",
         "CRUD feature in Rating",
         "Real-Time Recommendation",
       ],
+    },
+    {
+      title: "Recommendations Movies",
+      description:
+        "An application providing personalized Movies recommendations  using collaborative filtering methods, it delivers tailored suggestions based on user preferences and similar user behaviors,",
+      image: "/assets/sysrec-movie.png",
+      technologies: ["Streamlit", "Python", "collaborative filtering"],
+
+      category: ["Machine Learning", "Recomendation"],
+      demoUrl: "https://c283zuqsq3ip535yhxvfiy.streamlit.app/",
+      githubUrl:
+        "https://github.com/ahmadseloabadi/sysrec-movie-collaborative-filtering-",
+      features: ["Simple Desain", "Real-Time Recommendation"],
+    },
+    {
+      title: "BCA stock price Prediction",
+      description:
+        "This application is designed to predict the stock price of BCA up to 7 days in advance using the SVR method. It allows users, especially investors and stock traders, to gain insights into short-term price movements. By utilizing real-time data from Yahoo Finance",
+      image: "/assets/pred-bca.png",
+      technologies: ["Streamlit", "Python", "SVR"],
+
+      category: ["Machine Learning", "Prediction"],
+      demoUrl:
+        "https://prediksi-saham-bca-svr-trth2hfulzzhbnz8qwwbkz.streamlit.app/",
+      githubUrl: "https://github.com/ahmadseloabadi/prediksi-saham-BCA-SVR",
+      features: ["Simple Desain", "Real-time Prediction ", "Real-Time Data"],
     },
 
     {
@@ -59,7 +110,7 @@ const Projects = () => {
       category: ["Web App", "Interactive App"],
       demoUrl: "https://wefly.netlify.app/",
       githubUrl: "https://github.com/WeFly-Team/Fullstack-Web-Javascript",
-      features: ["Responsive Desain", "Interactive App"],
+      features: ["Responsive Desain", "Interactive App", "User-Friendly"],
     },
 
     {
@@ -71,7 +122,7 @@ const Projects = () => {
       category: ["Web App", "Single Page App"],
       demoUrl: "https://ahmadseloabadi.github.io/slicing-qiscus/",
       githubUrl: "https://github.com/ahmadseloabadi/slicing-qiscus",
-      features: ["Single Page App", "Simple Design"],
+      features: ["Single Page App", "Simple Design", "Responsive Desain"],
     },
     {
       title: "Cashier Landing Page",
@@ -82,7 +133,35 @@ const Projects = () => {
       category: ["Web App", "Single Page App"],
       demoUrl: "https://ahmadseloabadi.github.io/slicing-cashier-bwa/",
       githubUrl: "https://github.com/ahmadseloabadi/slicing-cashier-bwa",
-      features: ["Single Page App", "Simple Design"],
+      features: ["Single Page App", "Simple Design", "Responsive Desain"],
+    },
+    {
+      title: "Aspect Based Sentiment Female Daily App Reviews",
+      description:
+        "This project involves developing a classification system for reviews on Female Daily, focusing on sentiment and aspect classification using Support Vector Machine (SVM) and Latent Dirichlet Allocation (LDA) techniques. SVM is utilized for accurately classifying the sentiment of reviews (positive or neutral), while LDA is applied for identifying the main aspects within the reviews",
+      image: "/assets/ansen-femaledaily.png",
+      technologies: ["Streamlit", "Python", "LDA", "SVM"],
+      category: [
+        "Machine Learning",
+        "Analysis Sentiment",
+        "Aspect Based Sentiment",
+      ],
+      demoUrl:
+        "https://female-daily-svm-lda-ymnvtr9bd87xvvjqzjbxhb.streamlit.app/",
+      githubUrl: "https://github.com/ahmadseloabadi/FEMALE-DAILY-SVM-LDA",
+      features: ["High Accuration", "Detection New Data", "Simple Design"],
+    },
+    {
+      title: "Sentiment Analysis TIX ID App Reviews",
+      description:
+        "An application that analyzes user sentiment from Tix ID reviews on Google Play Store Using  KNN model and SMOTE for hanndling imbalance dataset, it classifies reviews as positive, negative, or neutral, helping improve service quality and user experience.",
+      image: "/assets/ansen-tixid.png",
+      technologies: ["Streamlit", "Python", "SMOTE", "KNN"],
+      category: ["Machine Learning", "Analysis Sentiment"],
+      demoUrl:
+        "https://sentiment-analysis-knn-spuhrzb6ozhzdvuska4q5w.streamlit.app/",
+      githubUrl: "https://github.com/ahmadseloabadi/sentiment-analysis-KNN",
+      features: ["High Accuration", "Detection New Data", "Simple Design"],
     },
   ];
 
